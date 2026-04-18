422d14b7de139093fe506cc59bb0a3aa8f7ba6f5
Zahra <zsam0695@sdsu.edu>
2026-04-17 22:20:06 -0700

    I started brainstorming on how to approach this function, and decided to first start off by following the handlings it needs like out-of-bounds and cell wall checks. I wrote two variables, N and M, to keep track of my maze height and width (rows, columns). Then wrote the out-of-bounds check to make sure r or c aren't less than 0 or more than the maze height and width, N, M.

--- main.cpp
+++ main.cpp
@@ -117,9 +117,22 @@ void printPath(pair<int,int> exitcell,
 // STUDENTS IMPLEMENT DFS HERE
 // Add arguments, return type, and logic
 // ----------------------------------------------------------
-// bool dfs(……) {
-//     // Your code here
-// }
+bool dfs(int r, int c,
+         const vector<vector<int>>& maze,
+         vector<vector<bool>>& visited,
+         vector<vector<int>>& parent_r,
+         vector<vector<int>>& parent_c,
+         int exit_r, int exit_c) {
+
+    int N = maze.size();
+    int M = maze[0].size();
+
+    //out-of-bounds check
+    if (r < 0 || r >= N)
+        return false;
+    if (c < 0 || c >= M)
+        return false;
+}
 
 
 // ----------------------------------------------------------
